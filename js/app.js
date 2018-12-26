3591ac378a6ffa149b9664f1fdeb712b9cb82a90
Yura Kushnir <kushnir.yura@gmail.com>
2018-12-26 11:18:46 +0200

    Basic version of game

--- js/app.js
+++ js/app.js
@@ -1,46 +1,106 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const startSpeed = 300;
+const canvasParams = {
+  width: 505,
+  height: 606,
+  cellStep: 83,
+  topOffset: 83 - 101
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomEnemyRow() {
+  return canvasParams.topOffset + randomInt(1, 4) * canvasParams.cellStep;
+}
+
+const Enemy = function(player) {
+  this.player = player;
+  this.spriteSize = { width: 101, height: 171 };
+  this.x = -this.spriteSize.width;
+  this.y = getRandomEnemyRow();
+  this.speed = startSpeed * Math.random();
+  this.sprite = 'images/enemy-bug.png';
+};
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+  this.x += this.speed * dt;
+  if (this.x > canvasParams.width) this.x = -this.spriteSize.width;
 
-// Draw the enemy on the screen, required method for game
+  if (this.checkCollision()) this.player.setStartPoint();
+};
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Enemy.prototype.checkCollision = function() {
+  if (
+    this.y > this.player.y - canvasParams.cellStep &&
+    this.y < this.player.y + canvasParams.cellStep &&
+    this.x > this.player.x - canvasParams.cellStep &&
+    this.x < this.player.x + canvasParams.cellStep
+  ) {
+    return true;
+  }
+  return false;
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+const Boy = function() {
+  this.spriteSize = { width: 101, height: 171 };
+  this.sprite = 'images/char-boy.png';
+  this.setStartPoint();
+};
+Boy.prototype.update = function() {};
+Boy.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Boy.prototype.handleInput = function(direction) {
+  switch (direction) {
+    case 'left':
+      this.x -= this.spriteSize.width;
+      if (this.x <= 0) {
+        this.x = 0;
+      }
+      break;
+    case 'up':
+      this.y -= canvasParams.cellStep;
+      if (this.y <= canvasParams.topOffset) {
+        this.setStartPoint();
+      }
+      break;
+    case 'right':
+      this.x += this.spriteSize.width;
+      if (this.x >= canvasParams.width) {
+        this.x -= this.spriteSize.width;
+      }
+      break;
+    case 'down':
+      this.y += canvasParams.cellStep;
+      if (this.y >= canvasParams.height - this.spriteSize.height) {
+        this.y -= canvasParams.cellStep;
+      }
+      break;
+    default:
+      break;
+  }
+};
+Boy.prototype.setStartPoint = function() {
+  this.x = this.spriteSize.width * 2;
+  this.y = canvasParams.topOffset + 5 * canvasParams.cellStep;
+};
 
+const player = new Boy();
+const allEnemies = [
+    new Enemy(player),
+    new Enemy(player),
+    new Enemy(player)
+];
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+document.addEventListener('keyup', e => {
+  let allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
